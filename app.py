469d64b879ef03ef8a4bd515b3773dcc3b8f2c48
Yousuf Shabibi <145971672+Shabibi2003@users.noreply.github.com>
2025-06-11 11:16:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -399,7 +399,7 @@ elif choose == "Site Overview":
     st.markdown("""
     Initally creating this as a portfolio website in the form of an extended resume, I came to discover the uniqueness of Streamlit as compared to typical front-end frameworks such as Angular and Bootstrap. Even though Streamlit is primarily used as a web application for dashboarding, its extensive features make it more aesthetically appealing to explore with as compared to alternatives such as Plotly and Shiny.
     
-    With the convenience of using Python as a beginner-friendly programming language, I have now decided to evolve this personal project into a time capsule - documenting key moments and achievements that I have attained since commencing my formal education at 7 years old. In addition, should I be successful in completing this project, I intend to provide my codes as open-source, so that other students can document their educational journey in a similar manner.
+    With the convenience of using Python as a beginner-friendly programming language, I have now decided to evolve this personal project into a time capsule - documenting key moments and achievements that I have attained since commencing my formal education at 7 years old.
 
 
     """)
